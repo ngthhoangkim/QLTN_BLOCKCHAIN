0359d0a0c341ff73a7715c66810999e19124261e
ngthhoangkim <nguyenthihoangkim07022004@gmail.com>
2025-03-11 13:48:19 +0700

    cài thêm taiwind css cho giao diện

--- fe/src/App.js
+++
@@ -1,9 +0,0 @@
-import "./css/App.css";
-
-function App() {
-  return <div className="App">
-    <p>hello</p>
-  </div>;
-}
-
-export default App;
---
+++ fe/src/App.jsx
@@ -0,0 +1,12 @@
+import './css/App.css'
+
+function App() {
+ 
+  return (
+    <div className="bg-blue-500 text-white p-5 text-center text-3xl font-bold">
+      Tailwind CSS đã hoạt động!
+    </div>
+  )
+}
+
+export default App
